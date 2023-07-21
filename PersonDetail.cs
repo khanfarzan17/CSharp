c716e6f7a3cb11f5b13b8f79f6a6ea7b1c369785
khanfarzan17 <116535467+khanfarzan17@users.noreply.github.com>
2023-07-21 20:30:10 +0530

    Add files via upload

---
+++ PersonDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDetails
+{
+    class PersonDetail
+    {
+        public string name;
+        public int age;
+        public string gender;
+        public int salry;
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDetails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonDetail p1 = new PersonDetail();
+            p1.name = "Farzan";
+            p1.age = 22;
+            p1.gender = "male";
+            p1.salary = 375000;
+
+            Console.WriteLine(" "+ p1.name);
+            Console.WriteLine(" " + p1.age);
+            Console.WriteLine(" " + p1.gender);
+            Console.WriteLine(" " + p1.salary);
+
+
+
+
+
+            Console.Read();
+
+        }
+    }
+}
